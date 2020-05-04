f553dd971b6e35fabf1fe7fd342602a5eeb379ed
yuvtorres <yuvtorres@gmail.com>
2020-05-04 14:27:42 +0200

    grafica todos los chats

--- src/routes/home.py
+++ src/routes/home.py
@@ -7,5 +7,10 @@ def home_present():
     return render_template('home.html')
 
 @app.route("/chat_sentiment")
-def get_image():
+def get_image_route():
     return send_file('templates/sentiment.png', mimetype='image/png')
+
+
+@app.route("/sentiment_graph")
+def get_image_all_sentiments():
+    return send_file('templates/all_sentiments.png', mimetype='image/png')
--- src/routes/sentiments.py
+++ src/routes/sentiments.py
@@ -77,11 +77,54 @@ def sentiment_chat(chat,graph=True):
 def all_sentiment():
     chats=list(db.chats.find({},{"_id":0,"chat":1}))
     sentiments=[ sentiment_chat(chat['chat'],False) for chat in chats ]
+    sent_dic={chat['chat']:[chat['Neg'],chat['Pos'],chat['compound'],chat['neu']]
+        for chat in sentiments}
 
-    print(sentiments[0])
-    return {"sentiment":sentiments[0]}
+    survey (sent_dic,['Neg','Pos','compund','neu'])
+    plt.savefig('src/templates/all_sentiments.png')
+    print(sent_dic['chat0'])
+    return {"sentiment":sent_dic['chat0']}
 
 
+## Function taked from example mapplotlib
+def survey(results, category_names):
+    """
+    Parameters
+    ----------
+    results : dict
+        A mapping from question labels to a list of answers per category.
+        It is assumed all lists contain the same number of entries and that
+        it matches the length of *category_names*.
+    category_names : list of str
+        The category labels.
+    """
+    labels = list(results.keys())
+    data = np.array(list(results.values()))
+    data_cum = data.cumsum(axis=1)
+    category_colors = plt.get_cmap('RdYlGn')(
+        np.linspace(0.15, 0.85, data.shape[1]))
+
+    fig, ax = plt.subplots(figsize=(9.2, 5))
+    ax.invert_yaxis()
+    ax.xaxis.set_visible(False)
+    ax.set_xlim(0, np.sum(data, axis=1).max())
+
+    for i, (colname, color) in enumerate(zip(category_names, category_colors)):
+        widths = data[:, i]
+        starts = data_cum[:, i] - widths
+        ax.barh(labels, widths, left=starts, height=0.5,
+                label=colname, color=color)
+        xcenters = starts + widths / 2
+
+        r, g, b, _ = color
+        text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
+        for y, (x, c) in enumerate(zip(xcenters, widths)):
+            ax.text(x, y, str(round(c,3)), ha='center', va='center',
+                    color=text_color)
+    ax.legend(ncol=len(category_names), bbox_to_anchor=(0, 1),
+              loc='lower left', fontsize='small')
+
+    return fig, ax
 
 
 
